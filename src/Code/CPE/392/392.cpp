2cee3b2c200ea56348f80c0f3253aeced24f8c11
CF\jacky.lee <jacky.lee@chenfull.com.tw>
2024-03-06 11:11:49 +0800

    update

---
+++ src/Code/CPE/392/392.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+//#define square(x) x * x
+
+main() {
+
+int x, y;
+    cin >> x >>y ;  
+
+    cout << "input=" << x << y << endl;
+    //printf("input=%s\n", x);
+    //printf("%s\n", y);
+
+    //printf("%d\n", square(z + 1));
+
+    
+}

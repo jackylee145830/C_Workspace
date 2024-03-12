9f7a6968bc8e9d41f5124ead6a9c51d0e8b2c613
CF\jacky.lee <jacky.lee@chenfull.com.tw>
2024-03-12 13:22:21 +0800

    update

---
+++ src/CPE/UVA10409/UVA10409.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+main()
+{
+    // UVA10409 CPE11019
+    // Die Game
+
+    // 輸出說明
+
+
+    // 思考處理
+
+
+    /*
+    Sample Input
+    1
+    north
+    3
+    north
+    east
+    south
+    0
+    Sample Output
+    5
+    1
+    */
+}

91c7fdfaa259fbeddf9e7520a2615345c4e2467a
CF\jacky.lee <jacky.lee@chenfull.com.tw>
2024-03-12 16:02:35 +0800

    UVA10409 finish

--- src/CPE/UVA10409/UVA10409.cpp
+++ src/CPE/UVA10409/UVA10409.cpp
@@ -11,9 +11,144 @@ main()
 
     // 輸出說明
 
-
     // 思考處理
+    // 先想一下數字怎麼跑，拿出盒子標示一下，試著依照input轉看看
 
+    // 輸入
+    int intputSize;
+    cin >> intputSize;
+    string intputValue[intputSize];
+    // 輸入
+    for (int i = 0; i < intputSize; i++)
+    {
+        cin >> intputValue[i];
+    }
+
+    // die初始數值
+    int die = 1;
+    // 處理
+    int cnt = 0;
+    while (cnt < intputSize)
+    {
+
+        if (die == 1)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 3;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 4;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 5;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 2;
+            }
+        }
+        else if (die == 2)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 3;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 4;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 1;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 6;
+            }
+        }
+        else if (die == 3)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 2;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 5;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 6;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 1;
+            }
+        }
+        else if (die == 4)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 5;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 2;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 6;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 1;
+            }
+        }
+        else if (die == 5)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 3;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 4;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 6;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 1;
+            }
+        }
+        else if (die == 6)
+        {
+            if (intputValue[cnt] == "east")
+            {
+                die = 3;
+            }
+            else if (intputValue[cnt] == "west")
+            {
+                die = 4;
+            }
+            else if (intputValue[cnt] == "north")
+            {
+                die = 2;
+            }
+            else if (intputValue[cnt] == "south")
+            {
+                die = 5;
+            }
+        }
+        cnt++;
+    }
+
+    cout << die << endl;
 
     /*
     Sample Input
